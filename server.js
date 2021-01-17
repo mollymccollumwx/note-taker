d30ad0e44c8adeb24484b3e714e7d82bd37e56eb
Molly <mollymccollumwx@gmail.com>
2021-01-17 15:26:13 -0500

    basic express server setup

---
+++ server.js
@@ -0,0 +1,27 @@
+//require Express
+const express = require("express");
+//require node.js path module 
+const path = require("path");
+
+//create an instance of Express
+const app = express();
+
+//allows Heroku to choose a PORT or uses local host 3000
+const PORT = process.env.PORT || 3000;
+
+//sets up path for public folder
+const PUBLIC_DIR = path.resolve(__dirname, "public");
+
+// MIDDLEWARE: Sets up the express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//add routes
+
+
+
+//listen on the PORT
+app.listen(PORT, function() {
+    console.log(`App listening on PORT: ` + PORT);
+  });
+
